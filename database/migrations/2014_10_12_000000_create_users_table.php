99484bb7ed178ce25046e741388c2d571c32bcd9
MirkomilSaitov2204 <mike_saitov@mail.ru>
2022-07-29 11:45:58 +0500

    first commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('image')->default('amdin/img/admin.jpg');
+            $table->string('dob')->default('01-01-1997');
+
+            $table->string('username');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->string('role')->default();
+            $table->enum('status', ['active', 'disable'])->default('active');
+            $table->rememberToken();
+
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->index(['username', 'email', 'role']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
